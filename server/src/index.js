24a0ddd9c8beec94f770c1613d76355697add2d7
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-27 13:14:44 +0300

    Change configs for start project

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,10 @@ const http = require('http');
 const controller = require('./socketInit');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = http.createServer(app);
 server.listen(PORT, () =>
-  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Example app listening on port ${PORT}!`),
 );
 controller.createConnection(server);
