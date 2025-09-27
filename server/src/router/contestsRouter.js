da99583a9c93d1bf45e11abee4918bf13f524cd8
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-27 16:04:35 +0300

    Refactor post/pay

---
+++ server/src/router/contestsRouter.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const basicMiddlewares = require('../middlewares/basicMiddlewares');
+//const hashPass = require('../middlewares/hashPassMiddle');
+const userController = require('../controllers/userController');
+const contestController = require('../controllers/contestController');
+const checkToken = require('../middlewares/checkToken');
+const validators = require('../middlewares/validators');
+//const chatController = require('../controllers/chatController');
+const upload = require('../utils/fileUpload');
+
+const contestsRouter = express.Router();
+
+//POST http://localhost:5000/contests
+contestsRouter.post(
+  '/',
+  checkToken.checkToken,
+  basicMiddlewares.onlyForCustomer,
+  upload.uploadContestFiles,
+  basicMiddlewares.parseBody,
+  validators.validateContestCreation,
+  userController.payment,
+);
+
+// contestsRouter.post(
+//   '/dataForContest',
+//   checkToken.checkToken,
+//   contestController.dataForContest,
+// );
+
+// contestsRouter.post(
+//   '/contests/byCustomer',
+//   checkToken.checkToken,
+//   contestController.getCustomersContests,
+// );
+
+module.exports = contestsRouter;
