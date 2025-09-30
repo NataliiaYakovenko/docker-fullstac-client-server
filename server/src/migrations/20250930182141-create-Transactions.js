a3828cf72e297c59b6bc211f4159ef303ad91901
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-30 21:57:42 +0300

    Add transaction migration

---
+++ server/src/migrations/20250930182141-create-Transactions.js
@@ -0,0 +1,45 @@
+const { Op } = require('sequelize');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      operationType: {
+        type: Sequelize.ENUM('INCOM', 'EXPENS'),
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+        allowNull:false,
+      },
+      summa: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+    await queryInterface.addConstraint('Transactions', {
+      fields:['suma'],
+      type: 'check',
+      where:{ summa:{ [ Op.gt ] : 0 } },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Transactions');
+  },
+};
