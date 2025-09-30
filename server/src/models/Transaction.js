1995ce0c87577a3d8a8511bafc0e0938716322ba
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-09-30 22:32:39 +0300

    Fix summa/ validate

--- server/src/models/Transaction.js
+++ server/src/models/Transaction.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
       summa: {
         type:DataTypes.DECIMAL,
         validate: {
-          gt: 0,
+          min: 0,
         },
       },
     },
